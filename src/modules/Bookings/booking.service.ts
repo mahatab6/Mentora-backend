1c6f7ba0c15e7421c2233ee6a4681b203112ecef
mahatav6 <mahatabcall56@gmail.com>
2026-02-01 17:19:51 +0600

    counter find

--- src/modules/Bookings/booking.service.ts
+++ src/modules/Bookings/booking.service.ts
@@ -1,10 +1,7 @@
 import { booking } from "../../../generated/prisma/client";
 import { prisma } from "../../lib/prisma";
 
-const postBooking = async (
-  payload: Omit<booking, "createdAt">,
-  id: string,
-) => {
+const postBooking = async (payload: Omit<booking, "createdAt">, id: string) => {
   const result = await prisma.$transaction(async (tx) => {
     const newBooking = await tx.booking.create({
       data: {
@@ -20,41 +17,57 @@ const postBooking = async (
 
     await tx.tutorAvailability.update({
       where: {
-        id: payload.id
+        id: payload.id,
       },
-      data: { status: "booked" }
+      data: { status: "booked" },
     });
 
-    return newBooking
-    
+    return newBooking;
   });
 
   return result;
 };
 
-
-const getBooking = async (id:string) => {
+const getBooking = async (id: string) => {
   const result = await prisma.booking.findMany({
     where: {
-      OR:[
-        {studentId: id},
-        {tutorId: id}
-      ],
+      OR: [{ studentId: id }, { tutorId: id }],
     },
     include: {
       tutor: {
         select: {
-          fullName: true
-        }
-      }
-    }
+          fullName: true,
+        },
+      },
+    },
+  });
 
-  })
+  const totalSessions = await prisma.booking.count({
+    where: {
+      OR: [{ studentId: id }, { tutorId: id }],
+    },
+  });
 
-  return result
-}
+  const totalSpend = await prisma.booking.aggregate({
+    where: {
+      OR: [{ studentId: id }, { tutorId: id }],
+    },
+    _sum: {
+      price: true,
+    },
+  });
+
+  const upcomingSessions = await prisma.booking.count({
+    where: {
+      OR: [{ studentId: id }, { tutorId: id }],
+      status: "upcoming",
+    },
+  });
+
+  return { result, totalSessions, totalSpend: totalSpend._sum.price ?? 0, upcomingSessions };
+};
 
 export const bookingService = {
   postBooking,
-  getBooking
+  getBooking,
 };
